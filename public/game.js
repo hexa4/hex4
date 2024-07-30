89854d5980357bc7d0aa4d4a7131a0216c2f934c
hexa4 <wunderleds@gmail.com>
2024-07-30 09:27:27 +0200

    Actualizar game.js

--- public/game.js
+++ public/game.js
@@ -351,12 +351,9 @@ console.log(`World coordinates: (${worldPoint.x}, ${worldPoint.y})`);
 
 
 
-const fixedText66 = this.add.text(10, 10, 'HOLA', { fontSize: '16px', fill: '#ffffff'  , resolution: dpi , fontFamily: 'Roboto' });
+const fixedText66 = this.add.text(worldPoint.x - (this.cameras.main.width / 2) / zoomFactor + 10, worldPoint.y - (this.cameras.main.height / 2) / zoomFactor + 1 , 'HOLA', { fontSize: '16px', fill: '#ffffff'  , resolution: dpi , fontFamily: 'Roboto' });
 fixedText66.setShadow(2, 2, 'blue', 5);
 
-fixedText66.setPosition( 	
-worldPoint.x - (this.cameras.main.width / 2) / zoomFactor + 10, 
-worldPoint.y - (this.cameras.main.height / 2) / zoomFactor + 10); 
 
 
 /*
